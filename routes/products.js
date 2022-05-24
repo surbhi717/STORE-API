4a58e708091f5347917c9b076c4d66c1d96732ae
surbhi717 <surbhi7495@gmail.com>
2022-05-24 22:53:17 +0530

    addling product schema

---
+++ routes/products.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const { getAllProducts, getAllProductsStatic } = require('../controllers/products')
+const router = express.Router()
+
+
+//const { getAllProducts, getAllProductsStatic } = require('../controllers/products')
+
+router.route('/').get(getAllProducts)
+router.route('/static').get(getAllProductsStatic)
+
+module.exports = router
